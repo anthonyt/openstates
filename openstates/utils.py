964963e3f5510b2c68b7ce1b7b9dcb662be1f8e1
Anthony Theocharis <anthony.theocharis@gmail.com>
2012-03-13 16:09:33 -0700

    Add logic for finding abiword executable in alternate locations.
    Neither the common installer for AbiWord on OSX nor MacPorts include an
    executable for abiword on the user's PATH. This simplifies the process
    for OSX users to get ODT dependant bill data importing properly.

--- openstates/utils.py
+++ openstates/utils.py
@@ -8,15 +8,48 @@ build of billy.
 import os
 import subprocess
 
+def _is_executable(loc):
+    """
+    Check if a given file path or file name represents an executable file.
+
+    Will check on the PATH if the filename does not include any / characters.
+    """
+    if os.sep in loc:
+        return os.access(loc, os.X_OK)
+    else:
+        for d in os.environ['PATH'].split(':'):
+            if _is_executable(os.sep.join((d, loc))):
+                return True
+    return False
+
+def _get_executable(common_locations):
+    """
+    Given a list of common names for a particular program, return the first
+    executable one.
+    """
+    executable_locations = [
+        loc for loc in common_locations if _is_executable(loc)
+    ]
+    if not executable_locations:
+        raise Exception('Could not find executable. Looked in: \n%s' % (
+            "\n".join(common_locations)
+        ))
+    return executable_locations[0]
+
 def _abiword_odt(in_file, format):
     """
     Use abiword to convert an ODT file to another format.
 
     See http://www.abisource.com/wiki/Command_line_options
     """
+    exec_path = _get_executable([
+        '/Applications/AbiWord.app/Contents/MacOS/AbiWord',
+        '/Applications/MacPorts/AbiWord.app/Contents/MacOS/AbiWord',
+        'abiword'
+    ])
     cwd = '/tmp/'
     out_file = cwd + '.'.join((os.path.basename(in_file), format))
-    subprocess.check_call('abiword --to=%s %s' % (out_file, in_file),
+    subprocess.check_call('%s --to=%s %s' % (exec_path, out_file, in_file),
                           shell=True, cwd=cwd)
     return out_file
 
